d4482a20996d442284b099e7e0a558a9fe164515
Davichet-e <48657914+Davichet-e@users.noreply.github.com>
2019-06-18 13:09:15 +0200

    Refactored all code

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -1,227 +1,216 @@
 package BlackJackImpl;
 
+//The Deck class has been created by @natedane, I just made a few changes.
+
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
-import BlackJackImpl.Deck.Card;
-import BlackJackImpl.Players.Player;
-import static BlackJackImpl.Players.getMyDeck;
 
 public class BlackJack {
-	private static boolean checkFirstGame = true;
-	private int Computer_points = 0;
-	private int counter_computer = 0;
-	private boolean isAnInt = false;
-	private Scanner ask_user = new Scanner(System.in);
-	private static Deck myDeck = getMyDeck();
-	private ArrayList<Card> Computer_cards = myDeck.getInitialCards();
-	private ArrayList<Player> NonReplayPlayers = new ArrayList<Player>(5);
-	private static ArrayList<Player> Players = new ArrayList<Player>(5);
+	private static Scanner askUser = new Scanner(System.in);
+	private static BlackJackHand dealerHand = new BlackJackHand();
+	private static List<Player> players = new ArrayList<Player>();
 
-	public BlackJack() {
-		if (checkFirstGame) {
-			System.out.println("How many people is it going to play? (0-5)");
-			int numberOfPeople = ask_user.nextInt();
-			ask_user.nextLine();
-			for (int j = 0; j < numberOfPeople; j++) {
-				System.out.println("Please, enter your name, Player" + (j + 1));
-				String name = ask_user.nextLine();
-				for (int i = 0; i < 5; i++) {
-					System.out.println("\nHow much money do you have?\n");
-					isAnInt = ask_user.hasNextInt();
-					if (isAnInt) {
-						int initialMoney = ask_user.nextInt();
-						ask_user.nextLine();
-						Players.add(j, new Player(name, initialMoney));
-						break;
-					} else {
-						ask_user.nextLine();
-						System.out.println("Please, use only numbers.\n");
-					}
+	public static void blackJack() {
+		startGame();
+		while (true) {
+			System.out.println("Game started");
+			System.out.println("The first card of the dealer is " + dealerHand.getCards().get(0));
+
+			for (Player player : players)
+				playerTurn(player);
+
+			dealerTurn();
+			endGame();
+			if (!nextGame())
+				break;
+
+		}
+	}
+
+	private static void startGame() {
+		int numberOfPeople = askNumberOfPeople();
+		askAndSetPlayerAtributes(numberOfPeople);
+	}
+
+	private static int askNumberOfPeople() {
+		int numberOfPeople = 0;
+		while (true) {
+			System.out.println("How many people are going to play? (0-5)");
+			try {
+				numberOfPeople = askUser.nextInt();
+				askUser.nextLine();
+
+				if (numberOfPeople > 5 || 1 < numberOfPeople) {
+					System.out.println("The number of people must be between 1 and 5");
+					continue;
 				}
+				break;
 
+			} catch (InputMismatchException ime) {
+				System.out.println("The number of people must be a number between 1 and 5");
+			}
+		}
+		return numberOfPeople;
+	}
+
+	private static void askAndSetPlayerAttributes(int numberOfPeople) {
+		for (int i = 1; i <= numberOfPeople; i++) {
+			System.out.println("Please, enter your name, Player" + i);
+			String name = askUser.nextLine();
+			while (true) {
+				System.out.println("\nHow much money do you have?\n");
+				try {
+					int initialMoney = askUser.nextInt();
+					askUser.nextLine();
+					if (initialMoney < 50) {
+						System.out.println("The initial money must be greater or equal than 50");
+						continue;
+					}
+					players.add(new Player(name, initialMoney));
+					break;
+				} catch (InputMismatchException ime) {
+					askUser.nextLine();
+					System.out.println("Please, use only numbers.\n");
+				}
 			}
 
-		} else
-			isAnInt = true;
-		Players.removeAll(Collections.singleton(null));
-		start_game();
+		}
 
 	}
 
-	private void checkPlayerBet(Player p) {
-		for (int i = 0; i < 5; i++) {
+	private static void askPlayerBet(Player player) {
+		while (true) {
 			System.out.println("What bet do you wanna make?\n");
-			isAnInt = ask_user.hasNextInt();
-			if (isAnInt) {
-				isAnInt = false;
-				p.setActualBet(ask_user.nextInt());
-				ask_user.nextLine();
-				if (p.getActualBet() > p.getActualMoney()) {
+			try {
+				int bet = askUser.nextInt();
+				askUser.nextLine();
+				if (bet > player.actualMoney) {
 					System.out.println("You cannot make a bet bigger than your actual money");
-				} else if (p.getActualBet() <= 0) {
+				} else if (bet <= 0) {
 					System.out.println("The bet must be greater than zero");
-				} else
+				} else {
+					player.actualBet = bet;
 					break;
-			} else {
-				ask_user.nextLine();
+				}
+
+			} catch (InputMismatchException ime) {
+				askUser.nextLine();
 				System.out.println("Please, use only numbers.\n");
 			}
 
 		}
 	}
 
-	private void PlayerTurn(Player p) {
-		System.out.println(p.getName() + ", your actual money is " + p.getActualMoney());
-		checkPlayerBet(p);
-		System.out.println(
-				"Your turn has started.\nYour cards are " + p.getCards().get(0) + " and " + p.getCards().get(1));
-		for (int j = 0; j < 100; j++) {
-			if (!player_win_lose_condition(p)) {
-				System.out.println("Do you wanna hit? (y/n)\n");
-				String question = ask_user.nextLine();
-				if (question.toLowerCase().trim().equals("y") || question.toLowerCase().trim().equals("yes")) {
-					p.dealCard();
-					System.out.println(p.getPoints());
-					for (int i = 0; i < 21; i++) {
-						if (i == 0)
-							System.out.print("Now, your cards are: ");
-						try {
-							System.out.print(p.getCards().get(i) + ", ");
-						} catch (Exception e) {
-							System.out.println();
-							break;
-						}
-					}
-				} else {
-					System.out.println(p.getName() + " standed.");
-					break;
-				}
-			} else
-				break;
-
-		}
-	}
-
-	public void start_game() {
-		Players.removeAll(NonReplayPlayers);
-		System.out.println("The game has started.\n");
-		System.out.println("The first card of the bank is " + Computer_cards.get(0));
-
-		for (Player p : Players) {
-			PlayerTurn(p);
-		}
-		computer_turn();
-
-	}
-
-	private void computer_turn() {
-		if (counter_computer == 0) {
-			System.out.println("Now it is the bank turn.");
-			counter_computer = 1;
-		}
-		Computer_points = Deck.getSum(Computer_cards);
-		for (int i = 0; i < 21; i++) {
-			if (i == 0)
-				System.out.print("The bank cards are: ");
-			try {
-				System.out.print(Computer_cards.get(i) + ", ");
-			} catch (Exception e) {
-				System.out.println();
-				break;
-			}
-		}
-		System.out.println();
-
-		if (!computer_win_lose_condition()) {
-			if (Computer_points < 17) {
-				Computer_cards.add(myDeck.dealCard());
-				computer_turn();
-			} else {
-				endGame();
-			}
-		} else {
-			endGame();
-		}
-
-	}
-
-	private boolean computer_win_lose_condition() {
-		if (Computer_points > 21) {
-			System.out.println("The bank busted. The game ended :)\n");
-			Computer_points = 1;
-			endGame();
+	private static boolean checkIfYes() {
+		String answer = askUser.nextLine().trim().toLowerCase();
+		if (answer.equals("yes") || answer.equals("y") || answer.equals("1"))
 			return true;
-		} else
-			return false;
+		return false;
 	}
 
-	private boolean player_win_lose_condition(Player p) {
-		if (p.getPoints() == 21) {
-			p.updateActualMoney(p.getActualBet());
+	private static boolean askIfHit() {
+		System.out.println("Do you wanna hit? (y/n)\n");
+		return checkIfYes();
+	}
+
+	private static boolean playerWinOrLose(Player player) {
+		boolean result = false;
+		if (player.getPoints() == 21) {
 			System.out.println("BLACKJACK!");
+			result = true;
+		} else if (player.getPoints() == 0) {
+			System.out.println("BUST!\nI'm afraid you lose this game :(\n");
+			result = true;
+		}
+		return result;
+	}
+
+	private static void playerTurn(Player player) {
+		System.out.println(player + ", your actual money is " + player.actualMoney);
+		askPlayerBet(player);
+		System.out.println("Your turn has started.\nYour cards are " + player.getCards().get(0) + " and "
+				+ player.getCards().get(1));
+		while (!playerWinOrLose(player)) {
+			if (askIfHit()) {
+				player.dealCard();
+				System.out.println("Now, your cards are: " + player.hand());
+			} else {
+				System.out.println(player + " standed.");
+				break;
+			}
+
+		}
+	}
+
+	private static boolean dealerLost() {
+		if (dealerHand.getPoints() == 0) {
+			System.out.println("The dealer busted. The game ended :)\n");
 			return true;
-		} else if (p.getPoints() > 21) {
-			p.updateActualMoney(-p.getActualBet());
-			p.LoserPoints();
-			System.out.println("Bust.\nI'm afraid you lose this game :(\n");
-			return true;
+		}
+		return false;
+	}
+
+	private static void dealerTurn() {
+		System.out.println("The first card of the bank is " + dealerHand.getCards().get(0) + " and "
+				+ dealerHand.getCards().get(1));
+		while (!dealerLost() && dealerHand.getPoints() < 17) {
+			System.out.println("The dealer is going to hit a card\n");
+			dealerHand.dealCard();
+			System.out.println("Now, the dealer cards are " + dealerHand);
+		}
+	}
+
+	private static void endGame() {
+		for (Player player : players) {
+			if (player.getPoints() == 21 || player.getPoints() > dealerHand.getPoints()) {
+				player.actualMoney += player.actualBet;
+				System.out.println(player + " won " + player.actualBet * 2 + "€. :)\n");
+			} else if (player.getPoints() == 0 || player.getPoints() < dealerHand.getPoints()) {
+				player.actualMoney -= player.actualBet;
+				System.out.println(player + " lost against the dealer :(\n");
+			} else
+				System.out.println(player + " it is a Tie :|");
+
+		}
+	}
+
+	private static boolean askIfNextGame(Player player) {
+		boolean playerResets = false;
+		String finalBalance = String.valueOf(player.actualMoney - player.initialMoney) + " €";
+		if (!finalBalance.contains("-"))
+			finalBalance = "+" + finalBalance;
+
+		if (player.actualMoney > 0) {
+			System.out.println(player + ", do you want to play again? (y/n)");
+			if (checkIfYes()) {
+				player.initializeAttributes();
+				playerResets = true;
+			} else
+				System.out.println("Thanks for playing, " + player + " your final balance is " + finalBalance);
+
 		} else
-			return false;
+			System.out.println(player + ", you have lost all your money, Thanks for playing\n");
+
+		return playerResets;
+
 	}
 
-	private void endGame() {
-		for (Player p : Players) {
-			if (p.getPoints() != 21) {
-				if (p.getPoints() > Computer_points) {
-					p.updateActualMoney(p.getActualBet());
-					System.out.println(p.getName() + " won " + p.getActualBet() * 2 + "€.\n");
-				} else if (p.getPoints() == Computer_points) {
-					System.out.println(p.getName() + ", it is a Tie!.\n");
-				} else {
-					System.out.println(p.getName() + " lost against the bank.\n");
-					p.updateActualMoney(-p.getActualBet());
-				}
-			} else {
-				System.out.println(p.getName() + " won " + p.getActualBet() * 2 + "€.\n");
-			}
+	private static boolean nextGame() {
+		List<Player> nonReplayPlayers = new ArrayList<>();
+		for (Player player : players) {
+			if (!askIfNextGame(player))
+				nonReplayPlayers.add(player);
 		}
-		reset();
-	}
+		players.removeAll(nonReplayPlayers);
 
-	private void reset() {
-		for (Player p : Players) {
-			String final_balance = String.valueOf(p.getActualMoney() - p.getInitialMoney());
-			if (!final_balance.contains("-"))
-				final_balance = "+" + final_balance;
-			if (p.getActualMoney() > 0) {
-				System.out.println(p.getName() + ", do you want to play again? (y/n)\n");
-				String decision = ask_user.nextLine();
-				if (decision.toLowerCase().trim().equals("y") || decision.toLowerCase().trim().equals("yes")) {
-					p.resetCards();
-					checkFirstGame = false;
-				} else {
-
-					System.out.println(
-							"Thanks for playing, " + p.getName() + ", your final balance is " + final_balance + "€\n");
-					NonReplayPlayers.add(p);
-					if (Players.isEmpty())
-						ask_user.close();
-
-				}
-			} else {
-				NonReplayPlayers.add(p);
-				if (Players.isEmpty())
-					ask_user.close();
-				System.out.println(p.getName() + ", you have lost all you money. Thanks for playing");
-
-			}
+		if (!players.isEmpty()) {
+			dealerHand.initializeAttributes();
+			return true;
 		}
-		Players.removeAll(NonReplayPlayers);
-		if (!Players.isEmpty()) {
-			@SuppressWarnings("unused")
-			BlackJack g = new BlackJack();
-		}
+
+		return false;
 	}
 
 }
